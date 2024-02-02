a65c6d6cdd9177c521786c03b834002d6931c425
Julian <j.roth@kaosnetz.de>
2024-02-02 15:12:42 +0100

    Day02 Part 2 solved

--- d2.py
+++ d2.py
@@ -4,6 +4,11 @@ Day 2: Cube Conundrum
 '''
 
 
+MAX_RED = 12
+MAX_GREEM = 13
+MAX_BLUE = 14
+
+
 def parse_input(filename: str) -> list[str]:
     """
     Parse the input file and return its content as a list of strings.
@@ -29,7 +34,7 @@ def get_color_values(filename: str) -> dict:
 
 
     Returns:
-        dict:  dictionary where keys are game IDs (int) and values are 
+        dict:  dictionary where keys are game IDs (int) and values are
                 dictionaries representing the maximum counts of each color
                 ('red', 'green', 'blue') in that game.
     """
@@ -51,7 +56,25 @@ def get_color_values(filename: str) -> dict:
     return game_dict
 
 
-def get_possible_id_sum_1(filename: str) -> int:
+def multiply_all(factors: tuple[int]) -> int:
+    """
+    Multiply all elements in a tuple.
+    This function takes a tuple of numbers as input and returns the product of
+    all the numbers in the tuple.
+
+    Args:
+        factors (tuple): A tuple of integers to be multiplied.
+
+    Returns:
+        int: The product of all integers in the input tuple.
+    """
+    result = 1
+    for factor in factors:
+        result *= factor
+    return result
+
+
+def get_possible_id_sum(filename: str) -> str:
     """
     Calculate the sum of game IDs where color constraints are satisfied.
 
@@ -60,16 +83,14 @@ def get_possible_id_sum_1(filename: str) -> int:
         information.
 
     Returns:
-        int: The sum of game IDs where the count of each color ('red',
+        str: The sum of game IDs where the count of each color ('red',
         'green', 'blue') is within the specified limits.
     """
-    max_red = 12
-    max_green = 13
-    max_blue = 14
+
     games = get_color_values(filename)
-    solutions = [(game['red'] <= max_red,
-                  game['green'] <= max_green, 
-                  game['blue'] <= max_blue)
+    solutions = [(game['red'] <= MAX_RED,
+                  game['green'] <= MAX_GREEM,
+                  game['blue'] <= MAX_BLUE)
                  for game in games.values()]
     solution_lst = []
     for i, outcome in enumerate(solutions):
@@ -80,11 +101,38 @@ def get_possible_id_sum_1(filename: str) -> int:
     return str((sum(solution_lst)))
 
 
-def main():
+def get_possible_power(filename: str) -> str:
+    """
+    Calculate the total possible power from the color values of games.
 
+    This function reads the color values of games from a file, multiplies the
+    color values of each game, and sums up these products to get the total
+    possible power.
+
+    Args:
+        filename (str): The name of the file containing the color values of
+        games.
+
+    Returns:
+        str: The total possible power as a string.
+    """
+    games = get_color_values(filename)
+    solutions = [(game['red'],
+                  game['green'],
+                  game['blue'])
+                 for game in games.values()]
+    power_lst = []
+    for game_values in solutions:
+        value = multiply_all(game_values)
+        power_lst.append(value)
+    return str(sum(power_lst))
+
+
+def main():
     input_file = "day02.txt"
     print("Day 2: Cube Conundrum")
-    print("Part 1: " + get_possible_id_sum_1(input_file))
+    print("Part 1: " + get_possible_id_sum(input_file))
+    print("Part 2: " + get_possible_power(input_file))
 
 
 if __name__ == '__main__':
